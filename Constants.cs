7732a58f81b415b79bc9a7451a5729fd697f4731
NazarBulbotko <76509279+NazarBulbotko@users.noreply.github.com>
2021-06-02 08:08:28 +0300

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApi
+{
+    public class Constants
+    {
+        public static string source = "https://api.currencyscoop.com";
+        public static string apiKey = "9d5f1e0ea4a9c792c68a48c1a4c7f343";
+    }
+}
